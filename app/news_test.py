9a3d0f34a6c24d75950a538601f41cfe74528e4a
Kisavi <deniskisavi@gmail.com>
2022-05-02 18:24:50 +0300

    create a test for the Sources class

--- app/news_test.py
+++ app/news_test.py
@@ -21,10 +21,30 @@ class HeadlinesTest(unittest.TestCase):
                                        '"https://images.wsj.net/im-535503/social', '2022-05-02T13:16:00Z')
 
     def test_instance(self):
-         self.assertTrue(isinstance(self.new_headlines, Headlines))
+        self.assertTrue(isinstance(self.new_headlines, Headlines))
 
 
+if __name__ == '__main__':
+    unittest.main()
 
+
+# test for the sources class
+class SourcesTest(unittest.TestCase):
+    """
+    Test case to test the behaviour of the Sorces class
+    """
+
+    def setUp(self):
+        """
+        This method runs before every case
+        """
+
+        self.new_sources = Sources("ABC News",
+                                   "Norges ledende nettavis med alltid oppdaterte nyheter innenfor innenriks, utenriks, sport og kultur.",
+                                   "https://www.aftenposten.no")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_sources, Sources))
 
 
 if __name__ == '__main__':
